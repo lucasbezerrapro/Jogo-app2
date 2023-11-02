b7a6018e980e90fa0cfd4d3cf155005798edec48
Lucas bezerra <140905557+lucasbezerrapro@users.noreply.github.com>
2023-11-02 16:30:14 -0300

    jogo finalizado

---
+++ jogo.js
@@ -0,0 +1,103 @@
+//Encontrar altura e largura da janela
+
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+var criaMosquitoTempo = 1500
+
+if (nivel === 'normal') {
+  //1500
+} else if(nivel === 'dificil') {
+  //1000
+} else if(nivel == 'chucknorris') {
+  //750
+}
+function ajustaTamanhoPalcoJogo() {
+  altura = window.innerHeight
+  largura = window.innerWidth
+
+  console.log(largura, altura);
+}
+ajustaTamanhoPalcoJogo()
+
+var cronometroo = setInterval(function() {
+  tempo-=1
+
+  if(tempo < 0) {
+    clearInterval(cronometro)
+    clearInterval(criaMosquito)
+    window.location.href = 'vitoria.html'
+  } else {
+  document.getElementById("cronometro").innerHTML = tempo
+  }
+}, 1000)
+function posicaoRandomica() {
+  
+  //remover o mosquito anterior (caso exista)
+  if(document.getElementById("mosquito")) {
+
+    if (vidas > 3) {
+      window.location.href = 'fim_de_jogo.html'
+    } else {
+    document.getElementById("mosquito").remove()
+
+    document.getElementById("v" + vidas).src="imagens/coracao_vazio.png"
+
+    vidas++
+    }
+  }
+
+  var posicaoX = Math.floor(Math.random() * largura) - 90
+  var posicaoY = Math.floor(Math.random() * altura) -90
+
+  posicaoX = posicaoX < 0 ? 0 : posicaoX
+  posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+  console.log(posicaoX,posicaoY);
+
+  //criar elemento HTML
+  var mosquito = document.createElement("img")
+  mosquito.src = 'imagens/mosquito.png'
+  mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+  mosquito.style.left = posicaoX + 'px'
+  mosquito.style.top = posicaoY + 'px'
+  mosquito.style.position = 'absolute'
+  mosquito.id = 'mosquito'
+  mosquito.onclick = function() {
+    this.remove()
+  }
+
+  document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+  var classe = Math.floor(Math.random() * 3)
+  
+  switch(classe) {
+    case 0:
+      return 'mosquito1'
+
+    case 1:
+      return 'mosquito2'
+
+    case 2:
+      return 'mosquito3'
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2)
+  
+  switch(classe) {
+    case 0:
+      return 'ladoA'
+
+    case 1:
+      return 'ladoB'
+  }
+}
